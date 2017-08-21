26c0d937169ae5246f6522a96d707a4fc347cc43
Geoffrey Lessel <geo@geoffreylessel.com>
2017-08-21 14:38:12 -0700

    Make the thing production/distribution ready

--- electron/webpack.config.js
+++ electron/webpack.config.js
@@ -1,16 +1,16 @@
 var webpack = require("webpack")
 module.exports = {
   entry: {
-    app: ["webpack/hot/dev-server", "./js/entry.js"],
+    app: ["webpack/hot/dev-server", "./js/entry.js"]
   },
   output: {
     // path: "./public/built",
     filename: "bundle.js",
-    publicPath: "http://localhost:8080/built/",
+    publicPath: "/"
   },
   devServer: {
     contentBase: "./public",
-    publicPath: "http://localhost:8080/built/",
+    publicPath: "http://localhost:8080/built/"
   },
   module: {
     loaders: [
@@ -20,13 +20,13 @@ module.exports = {
         exclude: /node_modules/,
         query: {
           presets: ["react", "es2015"],
-          plugins: ["transform-object-rest-spread"],
-        },
+          plugins: ["transform-object-rest-spread"]
+        }
       },
       { test: /\.css$/, loader: "style-loader!css-loader" },
-      { test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
-    ],
+      { test: /\.less$/, loader: "style-loader!css-loader!less-loader" }
+    ]
   },
   plugins: [new webpack.HotModuleReplacementPlugin()],
-  node: { fs: "empty" },
+  node: { fs: "empty" }
 }
